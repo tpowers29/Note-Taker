1496e6476e52f5049a1db05e3cf938b837019cb6
tpowers29 <113735451+tpowers29@users.noreply.github.com>
2022-12-13 11:42:36 -0500

    completed api routes

---
+++ routes/apiroutes.js
@@ -0,0 +1,35 @@
+const app = require("express").Router()
+let db = require("../db/db.json")
+const fs = require("fs")
+
+app.get("/api/notes",(req,res) => {
+    db =JSON.parse(fs.readFileSync("./db/db.json"))
+    res.json(db)
+})
+app.post("/api/notes",(req,res) => {
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: Math.floor(Math.radon()*9219)
+    }
+    db.push(newNote)
+   fs.writeFileSync("./db/db.json",JSON.stringify(db),function(err){
+    if(err) throw err
+   })
+    res.json(db)
+})
+app.delete("/api/notes/:id",(req,res) => {
+   let tempNotesList = []
+   for (let i= 0; i < db.length;i++){
+    if(db[i].id != req.params.id){
+        tempNotesList.push(db[i])
+    }
+   }
+    db = tempNotesList
+   fs.writeFileSync("./db/db.json",JSON.stringify(db),function(err){
+    if(err) throw err
+   })
+    res.json(db)
+})
+
+module.exports = app
